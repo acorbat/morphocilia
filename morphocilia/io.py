5812aece259be51dad8fb61386c880011aaa1437
Agustin Corbat <agustin.corbat@ki.se>
2024-03-20 16:58:44 +0100

    mend

--- morphocilia/io.py
+++ morphocilia/io.py
@@ -4,7 +4,8 @@ from skimage.exposure import rescale_intensity
 
 
 def load_rescaled_channel(filepath: Path, scene: str, channel: int):
-    """Loads a specific channel from a scene in a lif file and rescales it according to its own bit depth."""
+    """Loads a specific channel from a scene in a lif file and rescales it
+    according to its own bit depth."""
     file = AICSImage(filepath)
     file.set_scene(scene)
     return rescale_intensity(file.dask_data[0][channel])

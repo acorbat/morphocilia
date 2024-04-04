b8f46be0b00f725a79a6514413db212dcae729b5
Agustin Corbat <agustin.corbat@ki.se>
2024-04-04 16:21:27 +0200

    Add OPN segmentation to segmenter and notebook

--- morphocilia/segmenter.py
+++ morphocilia/segmenter.py
@@ -1,7 +1,16 @@
 import numpy as np
 from skimage.filters import threshold_otsu
+from skimage.morphology import binary_dilation
 
 
 def segment_cd13(cd13_channel: np.ndarray) -> np.ndarray:
     """Takes the CD13 channel stack and returns a mask for the CD13+ regions"""
     return cd13_channel > threshold_otsu(cd13_channel)
+
+
+def segment_opn(opn_channel: np.ndarray) -> np.ndarray:
+    """Takes the Osteopontin channel stack and returns a mask for the
+    OPN+ regions"""
+    opn_threshold = threshold_otsu(opn_channel)
+    opn_mask = opn_channel > opn_threshold
+    return binary_dilation(opn_mask)

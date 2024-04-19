76075f4c8ae1dd87c3ed2027f2ef05c89985e5b3
estela-mar22 <estelamar2001@gmail.com>
2024-04-19 12:22:20 +0200

    Upgraded classifier

--- morphocilia/classifier.py
+++ morphocilia/classifier.py
@@ -45,3 +45,22 @@ def scaler_classifier(df):
         X[["scaled_solidity", "scaled_axis_minor_length"]]
     )
     return X
+
+
+def scaler_classifier_2(df):
+    """
+    Scales and classifies cilia from a dataframe based on "solidity" and "axis_minor_length".
+    """
+    scaler = joblib.load("D:/estela/results/20240410/scaler_3.sca")
+    X = df[["solidity", "axis_minor_length"]]
+    scaled_features = scaler.transform(X)
+    X[["scaled_solidity", "scaled_axis_minor_length"]] = pd.DataFrame(
+        scaled_features, index=X.index, columns=X.columns
+    )
+    cilia_classifier = joblib.load(
+        "D:/estela/results/20240410/cilia_classifier_3.ml"
+    )
+    X["predicted"] = cilia_classifier.predict(
+        X[["scaled_solidity", "scaled_axis_minor_length"]]
+    )
+    return X

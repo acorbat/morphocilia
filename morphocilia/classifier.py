dc6b582bc82d8a8935c022510274f33a962f7bdc
estela-mar22 <estelamar2001@gmail.com>
2024-05-02 14:02:59 +0200

    update classifier.py

--- morphocilia/classifier.py
+++ morphocilia/classifier.py
@@ -1,10 +1,10 @@
 import joblib
-import pandas as pd
 
 
 def scale_columns(X, sca="scaler_3"):
     """
-    Previously trained scaler scales the properties "solidity" and "axis_minor_length" of a cilia-channel dataframe.
+    Previously trained scaler scales the properties "solidity" and
+    "axis_minor_length" of a cilia-channel dataframe.
     """
     scaler = joblib.load(f"D:/estela/results/20240410/{sca}.sca")
     return scaler.transform(X)
@@ -12,7 +12,8 @@ def scale_columns(X, sca="scaler_3"):
 
 def classify_cilia(scaled_X, ml="cilia_classifier_4"):
     """
-    Classifies cilia from a dataframe based on previously scaled "solidity" and "axis_minor_length".
+    Classifies cilia from a dataframe based on previously scaled
+    "solidity" and "axis_minor_length".
     """
     cilia_classifier = joblib.load(f"D:/estela/results/20240410/{ml}.ml")
     return cilia_classifier.predict(scaled_X)

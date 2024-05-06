b3de907edc45893c494e038af55f056aadcf002c
estela-mar22 <estelamar2001@gmail.com>
2024-05-06 16:53:18 +0200

    Added region_set function

--- morphocilia/region_mapper.py
+++ morphocilia/region_mapper.py
@@ -15,15 +15,21 @@ def parse_regions(region_number):
         raise ValueError
 
 
+def region_set(mask, intensity_image):
+    return set(intensity_image[mask])
+
+
 def get_regions(labelled_img, regionmap):
     props = regionprops_table(
         labelled_img,
         regionmap,
         properties=["label", "intensity_max"],
-        extra_properties=[set],
+        extra_properties=[region_set],
     )
 
     df = pd.DataFrame(props)
-    df = props.rename(columns={"intensity_max": "region", "set": "all_regions"})
+    df = df.rename(
+        columns={"intensity_max": "region", "region_set": "all_regions"}
+    )
     df["region"] = df["region"].apply(parse_regions)
     return df
